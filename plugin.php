8f5e02b7a64fa858a4b765c1465d8712c0a85262
Sajid Khan <sajid_19khan@yahoo.com>
2024-11-17 22:23:34 +0500

    new zip

--- plugin.php
+++ plugin.php
@@ -12,32 +12,32 @@
  * Version: 1.0.0
  */
 
-namespace BreakdanceCustomElements;
+namespace SajidCustomElements;
 
 use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
 
 add_action('breakdance_loaded', function () {
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
-        'BreakdanceCustomElements',
-        'element',
-        'Custom Elements',
-        false
+        'SajidCustomElements',
+        'Sajid element',
+        'Sajid Custom Elements',
+        false,
     );
 
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
-        'BreakdanceCustomElements',
-        'macro',
-        'Custom Macros',
+        'SajidCustomElements',
+        'Sajid macro',
+        'Sajid Custom Macros',
         false,
     );
 
     \Breakdance\ElementStudio\registerSaveLocation(
         getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
-        'BreakdanceCustomElements',
-        'preset',
-        'Custom Presets',
+        'SajidCustomElements',
+        'Sajid preset',
+        'Sajid Custom Presets',
         false,
     );
 },
